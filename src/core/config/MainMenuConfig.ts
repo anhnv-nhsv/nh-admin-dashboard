b321bf4f1aa1c6cdbaed7e12d12332ec3dcb5dfc
Phuc HH <phuchh@nhsv.vn>
2023-10-17 12:14:16 +0700

    Add setting in route

--- src/core/config/MainMenuConfig.ts
+++ src/core/config/MainMenuConfig.ts
@@ -89,106 +89,106 @@ export interface MenuItem {
 //     ],
 //   },
 // ];
-const MainMenuConfig: Array<MenuItem> = [
-  {
-    pages: [
-      {
-        heading: "dashboard",
-        route: "/dashboard",
-        keenthemesIcon: "element-11",
-        bootstrapIcon: "bi-app-indicator",
-      },
-    ],
-  },
-  {
-    heading: "apps",
-    route: "/apps",
-    pages: [
-      {
-        heading: "fileManager",
-        route: "/apps/file-manager",
-        keenthemesIcon: "folder",
-        bootstrapIcon: "bi-graph-up",
-      },
-      {
-        heading: "bannerManagement",
-        route: "/apps/banner-management",
-        keenthemesIcon: "abstract-43",
-        bootstrapIcon: "bi-graph-up",
-      },
-      {
-        heading: "pageManagement",
-        route: "/apps/page-management",
-        keenthemesIcon: "element-equal",
-        bootstrapIcon: "bi-graph-up",
-      },
-      {
-        sectionTitle: "news",
-        route: "/news",
-        keenthemesIcon: "abstract-17",
-        bootstrapIcon: "bi-graph-up",
-        sub: [
-          {
-            heading: "newsCategory",
-            route: "/apps/news/category",
-          },
-          {
-            heading: "newsList",
-            route: "/apps/news/list",
-          },
-        ],
-      },
-      // {
-      //   heading: "contact",
-      //   route: "/apps/contact",
-      //   keenthemesIcon: "abstract-19",
-      //   bootstrapIcon: "bi-graph-up",
-      // },
-      {
-        sectionTitle: "report",
-        route: "/report",
-        keenthemesIcon: "cheque",
-        bootstrapIcon: "bi-graph-up",
-        sub: [
-          {
-            heading: "reportCategory",
-            route: "/apps/report/category",
-          },
-          {
-            heading: "reportList",
-            route: "/apps/report/list",
-          },
-        ],
-      },
-      {
-        heading: "user",
-        route: "/apps/user-management",
-        keenthemesIcon: "people",
-        bootstrapIcon: "bi-graph-up",
-      },
-      {
-        sectionTitle: "setting",
-        route: "/setting",
-        keenthemesIcon: "setting-2",
-        bootstrapIcon: "bi-graph-up",
-        sub: [
-          {
-            heading: "Menu",
-            route: "/apps/setting/menu",
-          },
-          // {
-          //   heading: "Display",
-          //   route: "/apps/setting/display",
-          // },
-          // {
-          //   heading: "Setting",
-          //   route: "/apps/setting/custom",
-          // },
-        ],
-      },
-    ],
-  },
-];
+// export const MainMenuConfig: Array<MenuItem> = [
+//   {
+//     pages: [
+//       {
+//         heading: "dashboard",
+//         route: "/dashboard",
+//         keenthemesIcon: "element-11",
+//         bootstrapIcon: "bi-app-indicator",
+//       },
+//     ],
+//   },
+//   {
+//     heading: "apps",
+//     route: "/apps",
+//     pages: [
+//       {
+//         heading: "fileManager",
+//         route: "/apps/file-manager",
+//         keenthemesIcon: "folder",
+//         bootstrapIcon: "bi-graph-up",
+//       },
+//       {
+//         heading: "bannerManagement",
+//         route: "/apps/banner-management",
+//         keenthemesIcon: "abstract-43",
+//         bootstrapIcon: "bi-graph-up",
+//       },
+//       {
+//         heading: "pageManagement",
+//         route: "/apps/page-management",
+//         keenthemesIcon: "element-equal",
+//         bootstrapIcon: "bi-graph-up",
+//       },
+//       {
+//         sectionTitle: "news",
+//         route: "/news",
+//         keenthemesIcon: "abstract-17",
+//         bootstrapIcon: "bi-graph-up",
+//         sub: [
+//           {
+//             heading: "newsCategory",
+//             route: "/apps/news/category",
+//           },
+//           {
+//             heading: "newsList",
+//             route: "/apps/news/list",
+//           },
+//         ],
+//       },
+//       // {
+//       //   heading: "contact",
+//       //   route: "/apps/contact",
+//       //   keenthemesIcon: "abstract-19",
+//       //   bootstrapIcon: "bi-graph-up",
+//       // },
+//       {
+//         sectionTitle: "report",
+//         route: "/report",
+//         keenthemesIcon: "cheque",
+//         bootstrapIcon: "bi-graph-up",
+//         sub: [
+//           {
+//             heading: "reportCategory",
+//             route: "/apps/report/category",
+//           },
+//           {
+//             heading: "reportList",
+//             route: "/apps/report/list",
+//           },
+//         ],
+//       },
+//       {
+//         heading: "user",
+//         route: "/apps/user-management",
+//         keenthemesIcon: "people",
+//         bootstrapIcon: "bi-graph-up",
+//       },
+//       {
+//         sectionTitle: "setting",
+//         route: "/setting",
+//         keenthemesIcon: "setting-2",
+//         bootstrapIcon: "bi-graph-up",
+//         sub: [
+//           {
+//             heading: "Menu",
+//             route: "/apps/setting/menu",
+//           },
+//           // {
+//           //   heading: "Display",
+//           //   route: "/apps/setting/display",
+//           // },
+//           // {
+//           //   heading: "Setting",
+//           //   route: "/apps/setting/custom",
+//           // },
+//         ],
+//       },
+//     ],
+//   },
+// ];
 
 export const getMainMenu = (role): Array<MenuItem> => {
   return [
@@ -276,6 +276,27 @@ export const getMainMenu = (role): Array<MenuItem> => {
           accessible: "user" in role.grantedPermissions,
           keenthemesIcon: "people",
           bootstrapIcon: "bi-graph-up",
+        },
+        {
+          sectionTitle: "setting",
+          route: "/setting",
+          accessible: "setting" in role.grantedPermissions,
+          keenthemesIcon: "setting-2",
+          bootstrapIcon: "bi-graph-up",
+          sub: [
+            {
+              heading: "Menu",
+              route: "/apps/setting/menu",
+            },
+            // {
+            //   heading: "Display",
+            //   route: "/apps/setting/display",
+            // },
+            // {
+            //   heading: "Setting",
+            //   route: "/apps/setting/custom",
+            // },
+          ],
         },
       ],
     },
